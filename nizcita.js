6a3a9b1b6cff26bd9ffc5f966c6d48d59aea2240
Mujumdar <mujumdarn@tab110010.corpad.net.local>
2017-05-05 10:51:28 +1000

    added maintaining number of calls while flipped

--- nizcita.js
+++ nizcita.js
@@ -2,20 +2,38 @@ const cbuff = require('./limit-buffer')
 
 async function invoke(primary,alternate) {
   if(this.flipped) {
+    onInvokeAlternate.bind(this)
     return await alternate()
   }
-
+  onInvokePrimary.bind(this)()
   try {
     return await primary()
   } catch (err) {
-    this.buf.push({
-      error: err,
-      failureType: 'exception'
-    })
-    this.numberOfPrimaryFailures = this.numberOfPrimaryFailures + 1
-    this.flipped = this.shouldFlip(this.buf.getEnumerator(),this.numberOfPrimaryFailures)
+    onPrimaryError.bind(this)(err)
     return await alternate()
   }
+}
+
+function setShouldProbe(shouldProbe) {
+  this.shouldProbe = shouldProbe
+  return this
+}
+
+function onInvokeAlternate() {
+  this.numberOfCallsWhileFlipped++
+}
+
+function onInvokePrimary() {
+  this.numberOfCallsWhileFlipped = 0
+}
+
+function onPrimaryError(err) {
+  this.buf.push({
+    error: err,
+    failureType: 'exception'
+  })
+  this.numberOfPrimaryFailures = this.numberOfPrimaryFailures + 1
+  this.flipped = this.shouldFlip(this.buf.getEnumerator(),this.numberOfPrimaryFailures)
 }
 
 function alternate(alternative){
@@ -29,7 +47,12 @@ function circuitbreaker(bufferSize,shouldFlip) {
     buf: cbuff.create(bufferSize),
     shouldFlip: shouldFlip,
     invoke: invoke,
-    numberOfPrimaryFailures: 0
+    numberOfPrimaryFailures: 0,
+    numberOfCallsWhileFlipped: 0,
+    shouldProbe: () => {
+      return true
+    },
+    setShouldProbe: setShouldProbe,
   }
 }
 
